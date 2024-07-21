8508ee993fdafe7955f8086cb792de6213730e7b
Addy-cy <Adurapaul090@gmail.com>
2024-07-22 00:38:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Play background music when the page loads
-    playMusic();
-})
+    //Attempt to play background music automatically
+    const music =
+    document.getElementById('background-music');
+    music.play().catch(() => {
+        //if autoplay is blocked, prompt the users to click anywhere on the page to start the music
+
+        document.body.addEventListener('click',() => {
+            music.play();
+        }, { once: true});
+    });
+});
+
+    function playMusic() {
+
+        document.getElementById('background-music').play();
+    }
+
+    function pauseMusic() {
+        document.getElementById('background-music').pause();
+    }
