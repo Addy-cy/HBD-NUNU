4de6ba7866e91ab8ab2b8d45957c0c1412c91eb4
Addy-cy <Adurapaul090@gmail.com>
2024-07-22 01:26:52 +0100

    upd

--- script.js
+++ script.js
@@ -10,6 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
         }, { once: true});
     });
 });
+//modal functionality
+const modal = document.getElementById('image-modal');
+const modaling = document.getElementById('full-image');
+const close = document.querySelector('close');
+const galleryImages = document.querySelectorAll('gallery-img');
+galleryImages.forEach(image => {
+    image.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modaling.src = image.src;
+    });
+});
+close.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+//close out the modal when clicking outside of the image
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
 
     function playMusic() {
 
